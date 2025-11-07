df8f32ae60d28fd8ac2cb7c07cb0dc9235f4b3b2
Maria Vransiska P. C. T. D. P <167153554+MariaVransiska@users.noreply.github.com>
2025-11-07 18:45:24 +0700

    .

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -17,8 +17,9 @@ exports.handler = async (event, context) => {
   let cleanPath = path.replace('/.netlify/functions/server', '');
   if (!cleanPath.startsWith('/')) cleanPath = '/' + cleanPath;
 
-  const authHandler = require('../../auth');
-  const progressHandler = require('../../progress');
+  const authHandler = require('./auth.js');
+  const progressHandler = require('./progress.js');
+
 
   if (cleanPath.startsWith('/api/auth')) {
     return await authHandler.handler(event, context);
